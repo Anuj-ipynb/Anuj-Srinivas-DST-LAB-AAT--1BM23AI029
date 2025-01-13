0e9477a22a38115d39dfa9a933845a4869adcdbb
Anuj <anujsrinivas00@gmail.com>
2025-01-13 17:21:25 +0530

    Update QueueUsingTwoStacks.py

--- QueueUsingTwoStacks.py
+++ QueueUsingTwoStacks.py
@@ -1,38 +1,49 @@
-class QueueWTwoStacks:
+class QueueUsingTwoStacks:
     def __init__(self):
-        self.s1 = []  
-        self.s2 = []  
-
+        self.stack_in = []  
+        self.stack_out = []  
     def enqueue(self, value):
-        self.s1.append(value)
-
+        self.stack_in.append(value)
     def dequeue(self):
         self._shift_stacks()
-        if self.s2:
-            self.s2.pop()
-
+        if self.stack_out:
+            self.stack_out.pop()
     def front(self):
         self._shift_stacks()
-        if self.s2:
-            return self.s2[-1]
-
+        if self.stack_out:
+            return self.stack_out[-1]
     def _shift_stacks(self):
-        if not self.s2:
-            while self.s1:
-                self.s2.append(self.s1.pop())
-
+        if not self.stack_out:
+            while self.stack_in:
+                self.stack_out.append(self.stack_in.pop())
 if __name__ == "__main__":
     q = int(input().strip())  
-    queue = QueueWTwoStacks()
+    queue = QueueUsingTwoStacks()
 
     for _ in range(q):
         query = input().strip().split()
         command = int(query[0])
 
-        if command == 1:
+        if command == 1:  
             value = int(query[1])
             queue.enqueue(value)
-        elif command == 2:
+        elif command == 2:  
             queue.dequeue()
-        elif command == 3:
+        elif command == 3:  
             print(queue.front())
+
+#Test Case As per HackerRank
+10
+1 42
+2
+1 14
+3
+1 28
+3
+1 60
+1 78
+2
+2
+#Output
+14
+14
