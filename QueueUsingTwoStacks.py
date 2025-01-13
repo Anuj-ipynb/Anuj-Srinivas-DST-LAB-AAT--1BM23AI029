c2f0ef4509a9360adedcbfae853becb95bd20acb
Anuj <anujsrinivas00@gmail.com>
2025-01-13 10:00:16 +0530

    Create QueueUsingTwoStacks.py

---
+++ QueueUsingTwoStacks.py
@@ -0,0 +1,42 @@
+class QueueUsingTwoStacks:
+    def __init__(self):
+        self.stack_in = []  
+        self.stack_out = []  
+
+    def enqueue(self, value):
+        
+        self.stack_in.append(value)
+
+    def dequeue(self):
+        self._shift_stacks()
+        if self.stack_out:
+            self.stack_out.pop()
+
+    def front(self):
+        self._shift_stacks()
+        if self.stack_out:
+            return self.stack_out[-1]
+
+    def _shift_stacks(self):
+        
+        if not self.stack_out:
+            while self.stack_in:
+                self.stack_out.append(self.stack_in.pop())
+
+
+
+if __name__ == "__main__":
+    q = int(input().strip())  
+    queue = QueueUsingTwoStacks()
+
+    for _ in range(q):
+        query = input().strip().split()
+        command = int(query[0])
+
+        if command == 1:  
+            value = int(query[1])
+            queue.enqueue(value)
+        elif command == 2:  
+            queue.dequeue()
+        elif command == 3:  
+            print(queue.front())
