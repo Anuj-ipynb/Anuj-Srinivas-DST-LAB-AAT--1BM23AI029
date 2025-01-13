f0579a708ec78bf73e6b0bc08df2577446d4d175
Anuj <anujsrinivas00@gmail.com>
2025-01-13 16:58:55 +0530

    Update Game_of_two_stacks.py

--- Game_of_two_stacks.py
+++ Game_of_two_stacks.py
@@ -1,33 +1,31 @@
 def twoStacks(maxSum, a, b):
-    sum_current = 0
+    sum = 0
     count = 0
     i = 0
     j = 0
-    while i < len(a) and sum_current + a[i] <= maxSum:
-        sum_current += a[i]
+    while i < len(a) and sum + a[i] <= maxSum:
+        sum += a[i]
         i += 1
         count += 1
 
     max_count = count
     while j < len(b):
-        sum_current += b[j]
+        sum += b[j]
         j += 1
 
-        while sum_current > maxSum and i > 0:
+        while sum > maxSum and i > 0:
             i -= 1
-            sum_current -= a[i]
+            sum -= a[i]
             count -= 1
 
-        if sum_current <= maxSum:
+        if sum <= maxSum:
             max_count = max(max_count, i + j)
 
     return max_count
-if __name__ == "__main__":
-    g = int(input().strip())  # Number of games
-
-    for _ in range(g):
-        n, m, maxSum = map(int, input().strip().split())  
-        a = list(map(int, input().strip().split()))  
-        b = list(map(int, input().strip().split()))  
-        result = twoStacks(maxSum, a, b)
-        print(result)
+g = int(input().strip()) 
+for _ in range(g):
+    n, m, maxSum = map(int, input().strip().split())  
+    a = list(map(int, input().strip().split()))  
+    b = list(map(int, input().strip().split()))  
+    result = twoStacks(maxSum, a, b)
+    print(result)
